9055029ed240377c27854d139905a045fa94d612
Peter Valdemar Mørch <peter@morch.com>
2021-11-27 13:02:17 +0100

    Now sets appropriate links in SystemLandscapeView, SystemContextView and ContainerView

--- src/main/java/com/morch/c4viz/PumlGenerator.java
+++ src/main/java/com/morch/c4viz/PumlGenerator.java
@@ -3,20 +3,24 @@ package com.morch.c4viz;
 import com.structurizr.Workspace;
 import com.structurizr.dsl.StructurizrDslParser;
 import com.structurizr.io.Diagram;
-import com.structurizr.io.plantuml.AbstractPlantUMLExporter;
 import com.structurizr.io.plantuml.C4PlantUMLExporter;
-import com.structurizr.view.Styles;
-import com.structurizr.view.ThemeUtils;
+import com.structurizr.model.*;
+import com.structurizr.view.*;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PumlGenerator {
     String workspacePath;
     String outputPath;
+
+    // TODO: ask for an upstream setUrl(null) method.
+    static private Field urlFieldAccessor = null;
 
     PumlGenerator(String workspacePath, String outputPath) {
         this.workspacePath = workspacePath;
@@ -37,7 +41,6 @@ public class PumlGenerator {
         structurizrDslParser.parse(workspacePathFile);
         Workspace workspace;
         workspace = structurizrDslParser.getWorkspace();
-        long workspaceId = workspace.getId();
         ThemeUtils.loadThemes(workspace);
 
         addDefaultViewsAndStyles(workspace);
@@ -49,16 +52,156 @@ public class PumlGenerator {
         File outputDir = new File(outputPath);
         outputDir.mkdirs();
 
-        AbstractPlantUMLExporter plantUMLExporter = new C4PlantUMLExporter();
         if (workspace.getViews().isEmpty()) {
             throw new RuntimeException("the workspace contains no views");
         }
-        plantUMLExporter.setUseSequenceDiagrams(false);
-        Collection<Diagram> diagrams = plantUMLExporter.export(workspace);
+
+        Collection<Diagram> diagrams = export(workspace);
 
         for (Diagram diagram : diagrams) {
-            File file = new File(outputPath, String.format("%s-%s.puml", prefix(workspaceId), diagram.getKey()));
+            File file = new File(outputPath, String.format("%s.puml", getViewName(workspace, diagram.getView())));
             writeToFile(file, diagram.getDefinition());
+        }
+    }
+
+    String getViewName(Workspace workspace, View view) {
+        String prefix;
+        long workspaceId = workspace.getId();
+        if (workspaceId > 0) {
+            prefix = "structurizr-" + workspaceId;
+        } else {
+            prefix = "structurizr";
+        }
+
+        return String.format("%s-%s.puml", prefix, view.getKey());
+    }
+
+    private Collection<Diagram> export(Workspace workspace) {
+        C4PlantUMLExporter exporter = new C4PlantUMLExporter();
+        exporter.setUseSequenceDiagrams(false);
+        if (workspace == null) {
+            throw new IllegalArgumentException("A workspace must be provided.");
+        }
+
+        Collection<Diagram> diagrams = new ArrayList<>();
+
+        for (SystemLandscapeView view : workspace.getViews().getSystemLandscapeViews()) {
+            setSystemLandscapeElementURLs(workspace, view);
+            Diagram diagram = exporter.export(view);
+            resetElementURLs(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        for (SystemContextView view : workspace.getViews().getSystemContextViews()) {
+            setSystemContextElementURLs(workspace, view);
+            Diagram diagram = exporter.export(view);
+            resetElementURLs(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        for (ContainerView view : workspace.getViews().getContainerViews()) {
+            setContainerElementURLs(workspace, view);
+            Diagram diagram = exporter.export(view);
+            resetElementURLs(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        for (ComponentView view : workspace.getViews().getComponentViews()) {
+            Diagram diagram = exporter.export(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        for (DynamicView view : workspace.getViews().getDynamicViews()) {
+            Diagram diagram = exporter.export(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        for (DeploymentView view : workspace.getViews().getDeploymentViews()) {
+            Diagram diagram = exporter.export(view);
+            if (diagram != null) {
+                diagrams.add(diagram);
+            }
+        }
+
+        return diagrams;
+    }
+
+    private void setSystemLandscapeElementURLs(Workspace workspace, SystemLandscapeView view) {
+        Collection<SystemContextView> views = view.getViewSet().getSystemContextViews();
+        for (ElementView elementView : view.getElements()) {
+            Element element = elementView.getElement();
+            if (element instanceof SoftwareSystem) {
+                for (View alternateView : views) {
+                    if (alternateView.getSoftwareSystem() == element) {
+                        element.setUrl("https://view/" + getViewName(workspace, alternateView));
+                    }
+                }
+            }
+        }
+    }
+
+    private void setSystemContextElementURLs(Workspace workspace, SystemContextView view) {
+        Collection<ContainerView> views = view.getViewSet().getContainerViews();
+        for (ElementView elementView : view.getElements()) {
+            Element element = elementView.getElement();
+            if (element instanceof SoftwareSystem) {
+                for (View alternateView : views) {
+                    if (alternateView.getSoftwareSystem() == element) {
+                        element.setUrl("https://view/" + getViewName(workspace, alternateView));
+                    }
+                }
+            }
+        }
+    }
+
+    private void setContainerElementURLs(Workspace workspace, ContainerView view) {
+        Collection<ComponentView> views = view.getViewSet().getComponentViews();
+        for (ElementView elementView : view.getElements()) {
+            Element element = elementView.getElement();
+            if (element instanceof Container) {
+                for (ComponentView alternateView : views) {
+                    if (alternateView.getContainer() == element) {
+                        element.setUrl("https://view/" + getViewName(workspace, alternateView));
+                    }
+                }
+            }
+        }
+    }
+
+    private void resetElementURLs(View view) {
+        for (ElementView elementView : view.getElements()) {
+            Element element = elementView.getElement();
+            // TODO: ask for an upstream setUrl(null) method.
+            // Cannot set url to null with setUrl, so using reflection
+            // element.setUrl(null);
+            if (urlFieldAccessor == null) {
+                try {
+                    Class clazz = element.getClass();
+                    while (clazz != null && ! clazz.getSimpleName().equals("ModelItem")) {
+                        clazz = clazz.getSuperclass();
+                    }
+                    assert clazz != null;
+                    urlFieldAccessor = clazz.getDeclaredField("url");
+                    urlFieldAccessor.setAccessible(true);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                urlFieldAccessor.set(element, null);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -71,14 +214,6 @@ public class PumlGenerator {
         Styles styles = workspace.getViews().getConfiguration().getStyles();
         if (styles.getElements().isEmpty() && styles.getRelationships().isEmpty() && workspace.getViews().getConfiguration().getThemes() == null) {
             System.out.println(" - no styles or themes defined; use the \"default\" theme to add some default styles");
-        }
-    }
-
-    private String prefix(long workspaceId) {
-        if (workspaceId > 0) {
-            return "structurizr-" + workspaceId;
-        } else {
-            return "structurizr";
         }
     }
 
