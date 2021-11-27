9ddf90b0325ceae8ca55933194d1370655bd624f
Peter Valdemar Mørch <peter@morch.com>
2021-11-27 10:18:29 +0100

    Externalize input and output params

--- src/main/java/com/morch/c4viz/Main.java
+++ src/main/java/com/morch/c4viz/Main.java
@@ -1,9 +1,16 @@
 package com.morch.c4viz;
 
 public class Main {
+    private static void usage() {
+        System.err.println("Usage: input.dsl outputdir");
+        System.exit(1);
+    }
     public static void main(String[] args) {
-        String workspacePathAsString = "/home/peter/idea/c4/dsl/examples/big-bank-plc.dsl";
-        String outputPath = "/home/peter/idea/c4/foobar";
+        String workspacePathAsString = args[0];
+        String outputPath = args[1];
+        if (workspacePathAsString == null || outputPath == null) {
+            usage();
+        }
         PumlGenerator generator = new PumlGenerator(workspacePathAsString, outputPath);
         generator.generate();
     }
